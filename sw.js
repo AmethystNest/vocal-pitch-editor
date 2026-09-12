d388bccbeb9002b4d1dbd504440f56710e3d8eff
Retsu <tsuki1928@gmail.com>
2026-09-12 15:01:10 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,13 @@
+const CACHE='vocal-pitch-editor-v1';
+const SHELL=['./','./index.html','./manifest.webmanifest','./icon-180.png','./icon-192.png','./icon-512.png'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL))));
+self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
+self.addEventListener('fetch',e=>{
+  if(e.request.mode==='navigate'){
+    e.respondWith(fetch(e.request).then(r=>{
+      const copy=r.clone(); caches.open(CACHE).then(c=>c.put(e.request,copy)); return r;
+    }).catch(()=>caches.match('./index.html')));
+    return;
+  }
+  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+});
