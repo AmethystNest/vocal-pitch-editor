25873c72edbd4b8f0b5594c4eb55ee7cc6f7c2d9
Retsu <tsuki1928@gmail.com>
2026-09-24 11:41:28 +0900

    Refresh installed app shell for rotated inspector layering

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='vocal-pitch-editor-v52-narrow-phone-pitch-targets';
+const CACHE='vocal-pitch-editor-v53-rotated-inspector-layering';
 const SHELL=['./','./index.html','./manifest.webmanifest','./src/engine.js','./src/app.js','./src/worker.js','./icon-180.png','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil((async()=>{
   const cache=await caches.open(CACHE);
