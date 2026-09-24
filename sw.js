724e6d20b9867706435491c5f3b6fb34a2d3c933
Retsu <tsuki1928@gmail.com>
2026-09-24 10:49:44 +0900

    Refresh PWA cache for mobile toolbar cues

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='vocal-pitch-editor-v49-mp3-vbr-preflight';
+const CACHE='vocal-pitch-editor-v50-mobile-toolbar-scroll-cues';
 const SHELL=['./','./index.html','./manifest.webmanifest','./src/engine.js','./src/app.js','./src/worker.js','./icon-180.png','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil((async()=>{
   const cache=await caches.open(CACHE);
